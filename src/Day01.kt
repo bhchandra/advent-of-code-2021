1070bae8db8f54433bb42bee659b4c9792977e74
mitra.chandra <bhanumchandra@gmail.com>
2021-12-03 19:09:34 -0600

    AOC-2021: Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val ints = input.mapNotNull { it.toIntOrNull() }
+        var prev = 0
+        var count = -1
+        ints.forEach {
+            if (it > prev) count++
+            prev = it
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val ints = input.mapNotNull { it.toIntOrNull() }
+        var prevSum = 0
+        var count = -1
+        for (i in ints.indices) {
+            if (i == ints.size - 2) break
+            val sum = ints.slice(i..i + 2).sum()
+            if (sum > prevSum) count++
+            prevSum = sum
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_Test.txt")
+    println(part1(testInput))
+    println(part2(testInput))
 
-    val input = readInput("Day01")
+    val input = readInput("Day01_Input.txt")
     println(part1(input))
     println(part2(input))
 }
